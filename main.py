7daae76dd241b6385579e6cf47b449e81d762a0d
Aditya Mali <avmali21-cse@bvucoep.edu.in>
2022-02-06 17:57:16 +0530

    Exit statement added & adding states to csv

--- main.py
+++ main.py
@@ -13,19 +13,20 @@ correct = []
 allstates = states.state.tolist()
 while len(correct) <= 50:
     state_input = screen.textinput(title= f"{len(correct)}/50 States Correct" , prompt="Enter the name of a US state").title()
-    for name in allstates:
-        if state_input == name:
-            x = int(states.loc[states['state'] == state_input].x)
-            y = int(states.loc[states['state'] == state_input].y)
-            correct.append(name)
-            t.penup()
-            t.hideturtle()
-            t.goto(x, y)
-            t.pendown()
-            t.write(name)
-        else:
-            pass
-    print(len(correct))
-
-
-turtle.mainloop()
+    if state_input == "Exit":
+        break
+    if state_input in allstates:
+        x = int(states.loc[states['state'] == state_input].x)
+        y = int(states.loc[states['state'] == state_input].y)
+        correct.append(state_input)
+        t.penup()
+        t.hideturtle()
+        t.goto(x, y)
+        t.pendown()
+        t.write(state_input)
+    else:
+        pass
+tolearn = []
+for state in allstates:
+    if state not in correct:
+        tolearn.append(state)
